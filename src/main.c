f4498ee1a524fe9c903659e035114eccf850914a
zacharyranes <zachary.ranes@outlook.com>
2022-04-10 19:02:05 -0500

    Setup for real work

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include "chip8.h"
 
 int main(void)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Chip8 Window",
+    SDL_Window *window = SDL_CreateWindow(EMULATOR_WINDOW_TITLE,
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
-                                          640, 320, SDL_WINDOW_SHOWN);
+                                          CHIP8_WIDTH * CHIP8_WINDOWS_MULTIPLIER,
+                                          CHIP8_HEIGHT * CHIP8_WINDOWS_MULTIPLIER,
+                                          SDL_WINDOW_SHOWN);
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
